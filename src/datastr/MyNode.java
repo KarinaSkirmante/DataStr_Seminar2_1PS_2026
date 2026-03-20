804fd278ecf9dcc12e0b6033f839e29e041da817
KarinaSkirmante <karina.krinkele@gmail.com>
2026-03-20 18:33:09 +0200

    Edit: added variables, getters and setters in the MyNode class

--- src/datastr/MyNode.java
+++ src/datastr/MyNode.java
@@ -1,5 +1,28 @@
 package datastr;
 
 public class MyNode {
-
+	private char element;
+	private MyNode nextNode = null;
+	private MyNode previousNode = null;
+	
+	public char getElement() {
+		return element;
+	}
+	public MyNode getNextNode() {
+		return nextNode;
+	}
+	public MyNode getPreviousNode() {
+		return previousNode;
+	}
+	public void setElement(char element) {//TODO kad pāriesim uz TType, tad jāparabauda uz not null
+		this.element = element;
+	}
+	public void setNextNode(MyNode nextNode) {
+		this.nextNode = nextNode;
+	}
+	public void setPreviousNode(MyNode previousNode) {
+		this.previousNode = previousNode;
+	}
+	
+	
 }
